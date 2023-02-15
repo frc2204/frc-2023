88ac9b481fe5ba3070d08439a5d6858ac08a7fb5
James Ding <jamesding365@gmail.com>
2023-02-15 15:20:06 -0800

    Add kdocs to JsonDump.kt

--- src/main/java/org/crosspointacademy/frc/lib/limelight/json/JsonDump.kt
+++ src/main/java/org/crosspointacademy/frc/lib/limelight/json/JsonDump.kt
@@ -4,7 +4,13 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Serializer
 
+/**
+ * This class is used to deserialize the JSON dump from the Limelight.
+ */
 @Serializable
 data class JsonDump(
+    /**
+     * The actual results of the JSON dump. This is the only field that is actually used.
+     */
     @SerialName("Results") val results: Results
 )
