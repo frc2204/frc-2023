20b94b7ef8201e7fede0f0036c29f0af5122d58c
James Ding <jamesding365@gmail.com>
2023-03-02 16:49:05 -0800

    New CANCoder offsets

--- src/main/java/org/crosspointacademy/frc/config/SwerveModuleConfigurations.kt
+++ src/main/java/org/crosspointacademy/frc/config/SwerveModuleConfigurations.kt
@@ -10,9 +10,9 @@ enum class SwerveModuleConfigurations(
     val steerEncoderOffset: Rotation2d,
 ) {
 
-    FRONT_LEFT(1, 2, 3, Rotation2d.fromDegrees(if (Robot.real) 258.22 else 0.0)),
-    FRONT_RIGHT(4, 5, 6, Rotation2d.fromDegrees(if (Robot.real) 320.27 else 0.0)),
-    BACK_LEFT(7, 8, 9, Rotation2d.fromDegrees(if (Robot.real) 217.61 else 0.0)),
-    BACK_RIGHT(10, 11, 12, Rotation2d.fromDegrees(if (Robot.real) 203.027 else 0.0)),
+    FRONT_LEFT(1, 2, 3, Rotation2d.fromDegrees(if (Robot.real) 307.8 else 0.0)),
+    FRONT_RIGHT(4, 5, 6, Rotation2d.fromDegrees(if (Robot.real) 350.59 else 0.0)),
+    BACK_LEFT(7, 8, 9, Rotation2d.fromDegrees(if (Robot.real) 25.04 else 0.0)),
+    BACK_RIGHT(10, 11, 12, Rotation2d.fromDegrees(if (Robot.real) 323.34 else 0.0)),
 
 }
