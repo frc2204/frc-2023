408463d4e9d090021521839ada147de11e673b9e
James Ding <jamesding365@gmail.com>
2023-03-05 19:18:51 -0800

    Add SlewRateLimiters

--- src/main/java/org/crosspointacademy/frc/commands/SwerveTeleop.kt
+++ src/main/java/org/crosspointacademy/frc/commands/SwerveTeleop.kt
@@ -1,8 +1,11 @@
 package org.crosspointacademy.frc.commands
 
 import edu.wpi.first.math.MathUtil
+import edu.wpi.first.math.filter.SlewRateLimiter
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.wpilibj2.command.CommandBase
+import org.crosspointacademy.frc.config.Controls.DRIVE_STICK_SLEW_RATE
+import org.crosspointacademy.frc.config.Controls.ROTATION_STICK_SLEW_RATE
 import org.crosspointacademy.frc.config.Controls.STICK_DEADBAND
 import org.crosspointacademy.frc.config.Swerve.MAX_OMEGA
 import org.crosspointacademy.frc.config.Swerve.MAX_SPEED
@@ -19,10 +22,19 @@ class SwerveTeleop(
         addRequirements(SwerveSubsystem)
     }
 
+    private val translationalSlewRateLimiter = SlewRateLimiter(DRIVE_STICK_SLEW_RATE)
+    private val strafeSlewRateLimiter = SlewRateLimiter(DRIVE_STICK_SLEW_RATE)
+    private val rotationalSlewRateLimiter = SlewRateLimiter(ROTATION_STICK_SLEW_RATE)
+
     override fun execute() {
-        val translationValue = MathUtil.applyDeadband(translation(), STICK_DEADBAND)
-        val strafeValue = MathUtil.applyDeadband(strafe(), STICK_DEADBAND)
-        val rotationValue = MathUtil.applyDeadband(rotation(), STICK_DEADBAND)
+
+        val translationSlew = translationalSlewRateLimiter.calculate(translation())
+        val strafeSlew = strafeSlewRateLimiter.calculate(strafe())
+        val rotationSlew = rotationalSlewRateLimiter.calculate(rotation())
+
+        val translationValue = MathUtil.applyDeadband(translationSlew, STICK_DEADBAND)
+        val strafeValue = MathUtil.applyDeadband(strafeSlew, STICK_DEADBAND)
+        val rotationValue = MathUtil.applyDeadband(rotationSlew, STICK_DEADBAND)
 
         SwerveSubsystem.drive(
             Translation2d(translationValue, strafeValue).times(MAX_SPEED),
