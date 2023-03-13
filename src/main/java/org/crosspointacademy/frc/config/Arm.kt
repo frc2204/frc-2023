ca4243334d15eb1b3ecc2523344d4a6bb8a3b84b
James Ding <jamesding365@gmail.com>
2023-03-12 21:53:11 -0700

    Implement arm homing

--- src/main/java/org/crosspointacademy/frc/config/Arm.kt
+++ src/main/java/org/crosspointacademy/frc/config/Arm.kt
@@ -1,12 +1,42 @@
 package org.crosspointacademy.frc.config
 
+import com.revrobotics.CANSparkMax
 import org.crosspointacademy.lib.PIDFBuilder
+import org.crosspointacademy.lib.control.homing.HomingConfiguration
 
 object Arm {
 
     const val FIRST_JOINT_SPARK_ID = 13
     const val SECOND_JOINT_SPARK_ID = 14
 
-    val FIRST_JOINT_PID = PIDFBuilder(0.0, 0.0, 0.0, 0.0)
-    val SECOND_JOINT_PID = PIDFBuilder(0.0, 0.0, 0.0, 0.0)
+    val FIRST_JOINT_CONFIG = HomingConfiguration(
+        "First Joint",
+        0,
+        0.2,
+        0.3,
+        PIDFBuilder(0.0, 0.0, 0.0),
+        -0.3 to 0.3,
+        CANSparkMax.IdleMode.kBrake
+    )
+
+    val SECOND_JOINT_CONFIG = HomingConfiguration(
+        "Second Joint",
+        1,
+        0.2,
+        0.3,
+        PIDFBuilder(0.0, 0.0, 0.0),
+        -0.3 to 0.3,
+        CANSparkMax.IdleMode.kBrake
+    )
+
+
+    enum class Positions(
+        val jointOnePosition: Double,
+        val jointTwoPosition: Double,
+    ) {
+
+        HOME(0.0, 0.0),
+
+    }
+
 }
--- src/main/java/org/crosspointacademy/frc/subsystems/ArmSubsystem.kt
+++ src/main/java/org/crosspointacademy/frc/subsystems/ArmSubsystem.kt
@@ -1,42 +1,27 @@
 package org.crosspointacademy.frc.subsystems
 
-import com.revrobotics.CANSparkMax
-import com.revrobotics.CANSparkMaxLowLevel
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.SubsystemBase
-import org.crosspointacademy.frc.config.Arm.FIRST_JOINT_PID
+import org.crosspointacademy.frc.config.Arm
+import org.crosspointacademy.frc.config.Arm.FIRST_JOINT_CONFIG
 import org.crosspointacademy.frc.config.Arm.FIRST_JOINT_SPARK_ID
-import org.crosspointacademy.frc.config.Arm.SECOND_JOINT_PID
+import org.crosspointacademy.frc.config.Arm.SECOND_JOINT_CONFIG
 import org.crosspointacademy.frc.config.Arm.SECOND_JOINT_SPARK_ID
+import org.crosspointacademy.lib.control.homing.HomingNEOSparkMax
 
 object ArmSubsystem : SubsystemBase() {
 
-    private val firstJointSpark = CANSparkMax(FIRST_JOINT_SPARK_ID, CANSparkMaxLowLevel.MotorType.kBrushless)
-    private val secondJointSpark = CANSparkMax(SECOND_JOINT_SPARK_ID, CANSparkMaxLowLevel.MotorType.kBrushless)
+    val firstJoint = HomingNEOSparkMax(FIRST_JOINT_SPARK_ID, FIRST_JOINT_CONFIG)
+    val secondJoint = HomingNEOSparkMax(SECOND_JOINT_SPARK_ID, SECOND_JOINT_CONFIG)
 
-    init {
-        firstJointSpark.apply {
-            restoreFactoryDefaults()
+    fun setToPosition(position: Arm.Positions) {
+        firstJoint.position = position.jointOnePosition
+        secondJoint.position = position.jointTwoPosition
+    }
 
-            pidController.apply {
-                p = FIRST_JOINT_PID.proportional
-                i = FIRST_JOINT_PID.integral
-                d = FIRST_JOINT_PID.derivative
-            }
-
-            idleMode = CANSparkMax.IdleMode.kBrake
-        }
-
-        secondJointSpark.apply {
-            restoreFactoryDefaults()
-
-            pidController.apply {
-                p = SECOND_JOINT_PID.proportional
-                i = SECOND_JOINT_PID.integral
-                d = SECOND_JOINT_PID.derivative
-            }
-
-            idleMode = CANSparkMax.IdleMode.kBrake
-        }
+    override fun periodic() {
+        SmartDashboard.putBoolean("First Joint Homed", firstJoint.atHome)
+        SmartDashboard.putBoolean("Second Joint Homed", secondJoint.atHome)
     }
 
 }
