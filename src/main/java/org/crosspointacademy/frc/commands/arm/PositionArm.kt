d187feeca24aab142901abf57b2cee86b45f9471
James Ding <jamesding365@gmail.com>
2023-03-19 19:44:21 -0700

    Changes by winnie

--- src/main/java/org/crosspointacademy/frc/commands/arm/PositionArm.kt
+++ src/main/java/org/crosspointacademy/frc/commands/arm/PositionArm.kt
@@ -1,5 +1,6 @@
 package org.crosspointacademy.frc.commands.arm
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.CommandBase
 import org.crosspointacademy.frc.subsystems.ArmSubsystem
 
