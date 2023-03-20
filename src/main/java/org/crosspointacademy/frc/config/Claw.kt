0f7f995bc7093db77635f6dac6e606bc17b945dc
James Ding <jamesding365@gmail.com>
2023-03-19 19:43:35 -0700

    Add ClawSubsystem

---
+++ src/main/java/org/crosspointacademy/frc/config/Claw.kt
@@ -0,0 +1,9 @@
+package org.crosspointacademy.frc.config
+
+object Claw {
+
+    const val PNEUMATIC_HUB_ID = 1
+    const val FORWARD_CHANNEL = 8
+    const val REVERSE_CHANNEL = 9
+
+}
--- src/main/java/org/crosspointacademy/frc/subsystems/ClawSubsystem.kt
+++ src/main/java/org/crosspointacademy/frc/subsystems/ClawSubsystem.kt
@@ -1,15 +1,30 @@
 package org.crosspointacademy.frc.subsystems
 
+import edu.wpi.first.wpilibj.DoubleSolenoid
 import edu.wpi.first.wpilibj.PneumaticHub
 import edu.wpi.first.wpilibj2.command.SubsystemBase
+import org.crosspointacademy.frc.config.Claw.FORWARD_CHANNEL
+import org.crosspointacademy.frc.config.Claw.PNEUMATIC_HUB_ID
+import org.crosspointacademy.frc.config.Claw.REVERSE_CHANNEL
+import org.crosspointacademy.frc.config.Status
 
 object ClawSubsystem : SubsystemBase() {
 
-    private val pneumaticHub = PneumaticHub(15)
+    val pcm = PneumaticHub(PNEUMATIC_HUB_ID)
+    val solenoid = pcm.makeDoubleSolenoid(FORWARD_CHANNEL, REVERSE_CHANNEL)
 
     init {
-        pneumaticHub.enableCompressorDigital()
+        pcm.enableCompressorDigital()
     }
 
+    fun open() {
+        solenoid.set(DoubleSolenoid.Value.kForward)
+        StatusSubsystem.setStatus(Status.CLAW_OPEN)
+    }
+
+    fun close() {
+        solenoid.set(DoubleSolenoid.Value.kReverse)
+        StatusSubsystem.setStatus(Status.CLAW_CLOSE)
+    }
 
 }
