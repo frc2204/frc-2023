a421239bb57d065a14b6ed46ff75100a0cb89b49
James Ding <jamesding365@gmail.com>
2023-03-19 19:42:51 -0700

    Create LED interface
    See RevLib Driver Docs

---
+++ src/main/java/org/crosspointacademy/lib/led/Blinkin.kt
@@ -0,0 +1,35 @@
+package org.crosspointacademy.lib.led
+
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.motorcontrol.Spark
+
+class Blinkin(channel: Int) : Spark(channel) {
+
+    override fun disable() {
+        return
+    }
+
+    override fun stopMotor() {
+        return
+    }
+
+    fun set(pattern: BlinkinPattern) {
+        val currentAlliance = DriverStation.getAlliance()
+        var currentAlliancePattern = BlinkinPattern.RED
+        var oppositeAlliancePattern = BlinkinPattern.BLUE
+        if (currentAlliance == DriverStation.Alliance.Blue) {
+            currentAlliancePattern = BlinkinPattern.BLUE
+            oppositeAlliancePattern = BlinkinPattern.RED
+        }
+
+        val desiredPattern = when (pattern) {
+            BlinkinPattern.ALLIANCE -> currentAlliancePattern
+            BlinkinPattern.OPPOSITE_ALLIANCE -> oppositeAlliancePattern
+            else -> pattern
+        }
+
+        super.set(desiredPattern.pwmPower)
+
+    }
+
+}
