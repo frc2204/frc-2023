1b59f22835d724a6308fd51e9012ac44a621ddca
James Ding <jamesding365@gmail.com>
2023-03-19 19:41:29 -0700

    Place auto chooser on SmartDashboard

--- src/main/java/org/crosspointacademy/frc/commands/Autos.kt
+++ src/main/java/org/crosspointacademy/frc/commands/Autos.kt
@@ -2,6 +2,7 @@ package org.crosspointacademy.frc.commands
 
 import com.pathplanner.lib.auto.SwerveAutoBuilder
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import org.crosspointacademy.frc.config.Swerve.AUTO_ROTATIONAL_PID
 import org.crosspointacademy.frc.config.Swerve.AUTO_TRANSLATIONAL_PID
@@ -10,9 +11,14 @@ import org.crosspointacademy.frc.subsystems.SwerveSubsystem
 
 object Autos {
 
-    val autoModeChooser = SendableChooser<AutoMode>().apply {
+
+    private val autoModeChooser = SendableChooser<AutoMode>().apply {
         AutoMode.values().forEach { addOption(it.optionName, it) }
         setDefaultOption(AutoMode.DEFAULT.optionName, AutoMode.DEFAULT)
+    }
+
+    init {
+        SmartDashboard.putData("Auto Mode", autoModeChooser)
     }
 
     enum class AutoMode(val optionName: String, val pathName: String) {
