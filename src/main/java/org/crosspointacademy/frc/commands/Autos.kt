36d68e4d1fa5042705f86dbee6c69fcf3f1ce1a9
James Ding <jamesding365@gmail.com>
2023-03-20 14:56:43 -0700

    Autonomous event mapping

--- src/main/java/org/crosspointacademy/frc/commands/Autos.kt
+++ src/main/java/org/crosspointacademy/frc/commands/Autos.kt
@@ -4,15 +4,22 @@ import com.pathplanner.lib.auto.SwerveAutoBuilder
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
+import org.crosspointacademy.frc.commands.Autos.AutoEvents.pickupItem
+import org.crosspointacademy.frc.commands.Autos.AutoEvents.placeNode
+import org.crosspointacademy.frc.commands.claw.CloseClaw
+import org.crosspointacademy.frc.commands.claw.OpenClaw
+import org.crosspointacademy.frc.config.Arm
 import org.crosspointacademy.frc.config.Swerve.AUTO_ROTATIONAL_PID
 import org.crosspointacademy.frc.config.Swerve.AUTO_TRANSLATIONAL_PID
 import org.crosspointacademy.frc.config.Swerve.KINEMATICS
+import org.crosspointacademy.frc.subsystems.ArmSubsystem
 import org.crosspointacademy.frc.subsystems.SwerveSubsystem
 
 object Autos {
 
 
-    private val autoModeChooser = SendableChooser<AutoMode>().apply {
+    val autoModeChooser = SendableChooser<AutoMode>().apply {
         AutoMode.values().forEach { addOption(it.optionName, it) }
         setDefaultOption(AutoMode.DEFAULT.optionName, AutoMode.DEFAULT)
     }
@@ -23,9 +30,30 @@ object Autos {
 
     enum class AutoMode(val optionName: String, val pathName: String) {
         DEFAULT("Default", "Default Path"),
+        BALANCE_PATH("Balance Path", "Balance Path"),
+        RIGHT_PATH("Right Path", "Right Path"),
     }
 
-    private val EVENT_MAP = emptyMap<String, Command>()
+    object AutoEvents {
+        fun placeNode(positionCommand: Command) = SequentialCommandGroup(
+            CloseClaw(),
+            positionCommand,
+            OpenClaw()
+        )
+
+
+        val pickupItem = SequentialCommandGroup(
+            OpenClaw(),
+            ArmSubsystem.setTargetPosition(Arm.Positions.FLOOR),
+            CloseClaw(),
+            ArmSubsystem.setTargetPosition(Arm.Positions.HOME),
+        )
+    }
+
+    private val EVENT_MAP = mapOf(
+        "PLACE_THIRD_NODE" to placeNode(ArmSubsystem.setTargetPosition(Arm.Positions.THIRD_NODE)),
+        "PICKUP" to pickupItem
+    )
 
     val autoBuilder = SwerveAutoBuilder(
         { SwerveSubsystem.pose },
