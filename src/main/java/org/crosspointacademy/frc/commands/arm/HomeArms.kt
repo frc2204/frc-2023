6f637b2ea82580d830141a200749326cf8c6c248
James Ding <jamesding365@gmail.com>
2023-03-19 19:45:03 -0700

    Disable homing on first joint
    Temporary or unnecessary until second limit switch is placed.

--- src/main/java/org/crosspointacademy/frc/commands/arm/HomeArms.kt
+++ src/main/java/org/crosspointacademy/frc/commands/arm/HomeArms.kt
@@ -3,18 +3,17 @@ package org.crosspointacademy.frc.commands.arm
 import edu.wpi.first.wpilibj2.command.CommandBase
 import org.crosspointacademy.frc.subsystems.ArmSubsystem
 
-object HomeArms : CommandBase() {
+class HomeArms : CommandBase() {
 
-    private val firstJointHomeCommand = ArmSubsystem.firstJoint.home()
     private val secondJointHomeCommand = ArmSubsystem.secondJoint.home()
 
     override fun initialize() {
         addRequirements(ArmSubsystem)
-        firstJointHomeCommand.schedule()
+        ArmSubsystem.firstJoint.resetEncoder()
         secondJointHomeCommand.schedule()
     }
 
     override fun isFinished(): Boolean {
-        return firstJointHomeCommand.isFinished && secondJointHomeCommand.isFinished
+        return secondJointHomeCommand.isFinished
     }
 }
