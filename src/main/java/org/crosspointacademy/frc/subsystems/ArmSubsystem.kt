66c9563109aa369dcdfbce56abd6c6c67b65a60d
James Ding <jamesding365@gmail.com>
2023-03-22 14:18:09 -0700

    Add Trim offsets

--- src/main/java/org/crosspointacademy/frc/subsystems/ArmSubsystem.kt
+++ src/main/java/org/crosspointacademy/frc/subsystems/ArmSubsystem.kt
@@ -16,6 +16,9 @@ object ArmSubsystem : SubsystemBase() {
     val firstJoint = HomingNEOSparkMax(FIRST_JOINT_SPARK_ID, FIRST_JOINT_CONFIG)
     val secondJoint = HomingNEOSparkMax(SECOND_JOINT_SPARK_ID, SECOND_JOINT_CONFIG)
 
+    var firstJointOffset = 0.0
+    var secondJointOffset = 0.0
+
     var selectedPosition: Arm.Positions? = null
         set(value) {
             field = value
@@ -25,7 +28,31 @@ object ArmSubsystem : SubsystemBase() {
     fun setTargetPosition(position: Arm.Positions?): CommandBase = runOnce {
         selectedPosition = position
     }
-    
+
+    fun nextTargetPosition(): CommandBase = runOnce {
+        selectedPosition = when(selectedPosition) {
+            Arm.Positions.FLOOR_TIPPED -> Arm.Positions.FLOOR
+            Arm.Positions.FLOOR -> Arm.Positions.HOME
+            Arm.Positions.HOME -> Arm.Positions.FIRST_NODE
+            Arm.Positions.FIRST_NODE -> Arm.Positions.SECOND_NODE
+            Arm.Positions.SECOND_NODE -> Arm.Positions.THIRD_NODE
+            Arm.Positions.THIRD_NODE -> Arm.Positions.THIRD_NODE
+            else -> Arm.Positions.HOME
+        }
+    }
+
+    fun previousTargetPosition(): CommandBase = runOnce {
+        selectedPosition = when (selectedPosition) {
+            Arm.Positions.FLOOR_TIPPED -> Arm.Positions.FLOOR_TIPPED
+            Arm.Positions.FLOOR -> Arm.Positions.FLOOR_TIPPED
+            Arm.Positions.HOME -> Arm.Positions.FLOOR
+            Arm.Positions.FIRST_NODE -> Arm.Positions.HOME
+            Arm.Positions.SECOND_NODE -> Arm.Positions.FIRST_NODE
+            Arm.Positions.THIRD_NODE -> Arm.Positions.SECOND_NODE
+            else -> Arm.Positions.HOME
+        }
+    }
+
     override fun periodic() {
         SmartDashboard.putBoolean("First Joint Homed", firstJoint.atHome)
         SmartDashboard.putBoolean("Second Joint Homed", secondJoint.atHome)
